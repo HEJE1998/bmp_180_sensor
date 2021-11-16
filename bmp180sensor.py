5ba99a77b3290694faea4537ebc2e59b713a8229
HEJE1998 <JeldrikHemme98@web.dem>
2021-11-16 14:38:25 +0100

    Version 2: Ausgabezeit hinzugefügt

--- bmp180sensor.py
+++ bmp180sensor.py
@@ -4,13 +4,14 @@
 
 #Hardware: ESP32 und den Sensor BMP180 angeschlossen 
 
-#Version: 0.1
+#Version: 0.2
 
 #Importieren der Klassen 
 from bmp180 import BMP180
 from machine import I2C, Pin
+from time import sleep
 
-#Konfigurationene
+#Konfigurationen
 bus = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
 bmp180 = BMP180(bus)
 bmp180.oversample_sett = 2
@@ -24,7 +25,10 @@ druck = bmp180.pressure
 druck_1 = druck / 100
 ausgabewert_druck = round(druck_1, 2)
 
-#Ausgabebefehle
-print('Temperatur in Grad Celsius: {0}'.format(ausgabewert_temperatur))
-print('Luftdruck in Pascal: {0}'.format(ausgabewert_druck))
+#Ausgabebefehle: es werden alles 10s die Werte ausgegeben 
+while True:
+    print('Temperatur in Grad Celsius: {0}'.format(ausgabewert_temperatur))
+    print('Luftdruck in Pascal: {0}'.format(ausgabewert_druck))
+    sleep (10)
+    
 
